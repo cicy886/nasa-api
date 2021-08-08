42fc372b33214faf4bca0458e286862edbb5c80c
cicy886 <84603339+cicy886@users.noreply.github.com>
2021-08-08 12:57:43 -0700

    Update index-ui-logic.js

--- src/index-ui-logic.js
+++ src/index-ui-logic.js
@@ -2,6 +2,26 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import Nasa from './business-logic.js';
+
+function clearField(){
+  $('#userDate').val("");
+  $('.showPic').text("");
+}
 
 $(document).ready(function() {
+  $('#nasaDate').click(function() {
+    const date = $('#userDate').val();
+    $('#userDate').val('');
+    let promise = Nasa.getImages(date);
+
+    promise.then(function(response) {
+      const body = JSON.parse(response);
+      clearField();
+      $('.showPic').append(`<img src = ${body.hdurl} width="500" height="500">`);
+      $('.showPic').val('');
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error}`);
+    });
+  });
 });
