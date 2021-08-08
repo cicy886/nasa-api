ded0319a56b6424b965892ff859cb8155f7de707
cicy886 <84603339+cicy886@users.noreply.github.com>
2021-08-08 16:48:41 -0700

    Update business-logic.js

--- src/business-logic.js
+++ src/business-logic.js
@@ -1,8 +1,10 @@
-export default class Nasa {
+export class Nasa {
   static getImages(date) {
     return new Promise(function(resolve,reject) {
       let request = new XMLHttpRequest();
-      const url = `https://api.nasa.gov/planetary/apod?api_key=vqiUBDjfEb87JrTX1jp1vYN8UIviXAOGCzeJe7aY&date=${date}`;
+      const url = `https://api.nasa.gov/planetary/apod?api_key=7UsKmdbnAq9yWWFvOrZpxRxPdMpSNicIZPgxvghZ&date=${date}`;
+      // const earthUrl =`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${date}&api_key=7UsKmdbnAq9yWWFvOrZpxRxPdMpSNicIZPgxvghZ`;
+      // console.log(request);
     
       request.onload = function() {
         if (this.status === 200) {
@@ -13,6 +15,28 @@ export default class Nasa {
       };
     
       request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
+
+export class Mars {
+  static getImages(date) {
+    return new Promise(function(resolve,reject) {
+      let request = new XMLHttpRequest();
+      // const url = `https://api.nasa.gov/planetary/apod?api_key=7UsKmdbnAq9yWWFvOrZpxRxPdMpSNicIZPgxvghZ&date=${date}`;
+      const earthUrl =`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${date}&api_key=7UsKmdbnAq9yWWFvOrZpxRxPdMpSNicIZPgxvghZ`;
+      // console.log(request);
+    
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
+        }
+      };
+    
+      request.open("GET", earthUrl, true);
       request.send();
     });
   }
