c197dc092131440388d9c98a1d6dc32e9564a92d
cicy886 <84603339+cicy886@users.noreply.github.com>
2021-08-08 16:48:14 -0700

    Update index-ui-logic.js

--- src/index-ui-logic.js
+++ src/index-ui-logic.js
@@ -2,24 +2,37 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import Nasa from './business-logic.js';
+import {Nasa , Mars} from './business-logic.js';
 
 function clearField(){
   $('#userDate').val("");
   $('.showPic').text("");
+  $('.showSecPic').text('');
 }
 
 $(document).ready(function() {
   $('#nasaDate').click(function() {
     const date = $('#userDate').val();
+    clearField();
     $('#userDate').val('');
     let promise = Nasa.getImages(date);
+    let promise1 = Mars.getImages(date);
 
     promise.then(function(response) {
       const body = JSON.parse(response);
-      clearField();
+      // clearField();
       $('.showPic').append(`<img src = ${body.hdurl} width="500" height="500">`);
+      // $('.showPic').append(`<img src = ${body.photos[0].img_src} width="500" height="500">`);
       $('.showPic').val('');
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error}`);
+    });
+
+    promise1.then(function(response){
+      const body = JSON.parse(response);
+      // clearField();
+      $('.showSecPic').append(`<img src = ${body.photos[0].img_src} width="500" height="500">`);
+      $('.showSecPic').val('');
     }, function(error) {
       $('.showErrors').text(`There was an error processing your request: ${error}`);
     });
